7afbc70cf8957818161ddb5cce3a11b775ee4537
Fei Du <fei.du@example.com>
2015-07-11 21:56:27 +0800

    Update hello.cpp
    add print() function to print string

--- hello.cpp
+++ hello.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+void print(char *);
+
 int main() {
     cout << "Hello World!" << endl;
 
+    char * mystr = (char *)"This is function test." ;
+    print(mystr);
+
     return 0;
+}
+
+void print(char * str) {
+    cout << str << endl;
 }
