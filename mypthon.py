6312ed7fa7dee33aafa805bc4992b3381a16039b
Fei Du <fei.du@example.com>
2015-07-12 00:50:41 +0800

    update mypthon, change code format

--- mypthon.py
+++ mypthon.py
@@ -5,9 +5,9 @@ print("hello world! --python")
 def my_add(x, y):
     return x + y
 
-print "1+2 = ", my_add(1,2)
-
 def my_mul(x, y):
     return x * y
 
 print "2 * 2 = ", my_mul(2,2)
+print "1 + 2 = ", my_add(1,2)
+
